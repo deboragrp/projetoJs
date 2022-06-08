6d0bd1d90d42a5a89c38474159db247bcc7733ed
deboragrp <104926904+deboragrp@users.noreply.github.com>
2022-06-08 13:59:50 -0300

    Add files via upload

--- jokenpo.js
+++ jokenpo.js
@@ -1,24 +1,48 @@
-let userInput = prompt("escolha : pedra, papel ou tesoura.")
-
 const getUserChoice = (userInput) => {
-    userInput = userInput.toLowerCase();
-    if (userInput === 'pedra'   userInput === 'papel'   userInput === 'tesoura') {
-    } 
-    else {
-        console.log('Escolha inválida');
-    }
+  if (userInput.toLocaleLowerCase() === "pedra") {
+    userInput = "pedra";
+  } else if (userInput.toLocaleLowerCase() === "papel") {
+    userInput = "papel";
+  } else if (userInput.toLocaleLowerCase() === "tesoura") {
+    userInput = "tesoura";
+  } else {
+    console.log(
+      "A opção escolhida não existe porfavor volte e escolha entre: pedra, papel ou tesoura!!"
+    );
+  }
+  return userInput;
+};
 
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "pedra";
+  } else if (randomNumber === 1) {
+    return "papel";
+  } else {
+    return "tesoura";
+  }
 }
- getUserChoice(userInput)
 
-function  getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {    
-        return 'pedra';
-    } else if (randomNumber === 1) {
-        return 'papel';
-    } else {        
-        return 'tesoura';
-    }
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice == computerChoice) {
+    return "Empate";
+  } else if (userChoice == "pedra" && computerChoice == "tesoura") {
+    return "Você ganhou";
+  } else if (userChoice == "papel" && computerChoice == "pedra") {
+    return "Você ganhou";
+  } else if (userChoice == "tesoura" && computerChoice == "papel") {
+    return "Você ganhou";
+  } else {
+    return "Você perdeu";
+  }
 }
 
+function playGame() {
+  let userChoice = getUserChoice(prompt("escolha : pedra, papel ou tesoura."));
+  let computerChoice = getComputerChoice();
+  console.log("Usuário escolheu: " + userChoice);
+  console.log("Computador escolheu: " + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+playGame();
